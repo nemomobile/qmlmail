nemomobile/qmlmail

commit a7efd42298aec5550a77149eb480eaf48bc8ec4e

[Settings] MeeGo.Components conversion WIP
Converted DropDownControl. This resulted in a change to the backend
model API: "pop3" and "imap4" have been replaced by "0" and "1".

This also introduces a bug since the new DropDown does not allow
setting the selectedIndex.
See https://bugs.meego.com/show_bug.cgi?id=16623

plugin/emailaccount.cpp
@@ -364,23 +364,33 @@ void EmailAccount::setPassword(QString val)
 
 QString EmailAccount::recvType() const
 {
-    return mRecvType;
+    if (mRecvType == "pop3")
+        return "0";
+    else if (mRecvType == "imap4")
+        return "1";
+    else
+        return QString();
 }
 
 void EmailAccount::setRecvType(QString val)
 {
     // prevent bug where recv type gets reset
     // when loading the first time
-    if (val != mRecvType) {
+    QString newRecvType;
+    if (val == "0")
+        newRecvType = "pop3";
+    else if (val == "1")
+        newRecvType = "imap4";
+    if (newRecvType != mRecvType) {
         mAccountConfig->removeServiceConfiguration(mRecvType);
-        mAccountConfig->addServiceConfiguration(val);
-        mRecvType = val;
+        mAccountConfig->addServiceConfiguration(newRecvType);
+        mRecvType = newRecvType;
         delete mRecvCfg;
         mRecvCfg = new QMailServiceConfiguration(mAccountConfig, mRecvType);
         mRecvCfg->setType(QMailServiceConfiguration::Source);
         mRecvCfg->setVersion(100);
         /*
-        if (val == "imap4") {
+        if (newRecvType == "imap4") {
             mRecvCfg->setValue("authentication", "0");
             mRecvCfg->setValue("autoDownload", "0");
             mRecvCfg->setValue("baseFolder", "");

plugin/emailaccountsettingsmodel.cpp
@@ -149,7 +149,12 @@ QVariant EmailAccountSettingsModel::data(const QModelIndex &index, int role) con
                 }
                 break;
             case RecvTypeRole:
-                return recvsvc;
+                if (recvsvc == "pop3")
+                    return 0;
+                else if (recvsvc == "imap4")
+                    return 1;
+                else
+                    return QVariant();
                 break;
             case RecvServerRole:
                 svccfg = mAccountConfigs[index.row()].serviceConfiguration(recvsvc);
@@ -211,6 +216,7 @@ bool EmailAccountSettingsModel::setData(const QModelIndex &index, const QVariant
         QMailAccountConfiguration::ServiceConfiguration svccfg;
         QStringList services;
         QString recvsvc;
+        QString newrecvsvc;
         //determine receiving protocol
         services = mAccountConfigs[index.row()].services();
         if (services.contains("imap4")) {
@@ -252,11 +258,16 @@ bool EmailAccountSettingsModel::setData(const QModelIndex &index, const QVariant
             case RecvTypeRole:
                 // prevent bug where recv type gets reset
                 // when loading the first time
-                if (value.toString() == recvsvc) {
+                if (value.toInt() == 0) {
+                    newrecvsvc = "pop3";
+                } else if (value.toInt() == 1) {
+                    newrecvsvc = "imap4";
+                }
+                if (newrecvsvc == recvsvc) {
                     return true;
                 } else {
                     mAccountConfigs[index.row()].removeServiceConfiguration(recvsvc);
-                    mAccountConfigs[index.row()].addServiceConfiguration(value.toString());
+                    mAccountConfigs[index.row()].addServiceConfiguration(newrecvsvc);
                     getRecvCfg(mAccountConfigs[index.row()])->setValue("encryption", "1"); // SSL
                     getRecvCfg(mAccountConfigs[index.row()])->setValue("servicetype", "source");
                     getRecvCfg(mAccountConfigs[index.row()])->setValue("version", "100");

settings/AccountExpandobox.qml
@@ -127,9 +127,9 @@ Expandobox {
                 Item { width: 1; height: 1; },   // spacer
                 DropDownControl {
                     label: qsTr("Server type")
-                    dataList: Settings.serviceModel
-                    selectedValue: Settings.serviceName(model.recvType)
-                    onSelectionChanged: accountSettingsModel.setDataWrapper(listIndex, Settings.serviceCode(data), 38)
+                    model: Settings.serviceModel
+                    selectedIndex: model.recvType
+                    onTriggered: accountSettingsModel.setDataWrapper(listIndex, index, 38)
                 },
                 TextControl {
                     label: qsTr("Server address")
@@ -145,9 +145,9 @@ Expandobox {
                 },
                 DropDownControl {
                     label: qsTr("Security")
-                    dataList: Settings.encryptionModel
-                    selectedValue: Settings.encryptionName(model.recvSecurity)
-                    onSelectionChanged: accountSettingsModel.setDataWrapper(listIndex, Settings.encryptionCode(data), 41)
+                    model: Settings.encryptionModel
+                    selectedIndex: model.recvSecurity
+                    onTriggered: accountSettingsModel.setDataWrapper(listIndex, index, 41)
                 },
                 TextControl {
                     label: qsTr("Username")
@@ -182,15 +182,15 @@ Expandobox {
                 },
                 DropDownControl {
                     label: qsTr("Authentication")
-                    dataList: Settings.authenticationModel
-                    selectedValue: Settings.authenticationName(model.sendAuth)
-                    onSelectionChanged: accountSettingsModel.setDataWrapper(listIndex, Settings.authenticationCode(data), 46)
+                    model: Settings.authenticationModel
+                    selectedIndex: model.sendAuth
+                    onTriggered: accountSettingsModel.setDataWrapper(listIndex, index, 46)
                 },
                 DropDownControl {
                     label: qsTr("Security")
-                    dataList: Settings.encryptionModel
-                    selectedValue: Settings.encryptionName(model.sendSecurity)
-                    onSelectionChanged: accountSettingsModel.setDataWrapper(listIndex, Settings.encryptionCode(data), 47)
+                    model: Settings.encryptionModel
+                    selectedIndex: model.sendSecurity
+                    onTriggered: accountSettingsModel.setDataWrapper(listIndex, index, 47)
                 },
                 TextControl {
                     label: qsTr("Username")

settings/DropDownControl.qml
@@ -7,7 +7,7 @@
  */
 
 import Qt 4.7
-import MeeGo.Labs.Components 0.1
+import MeeGo.Components 0.1
 
 Column {
     id: root
@@ -16,10 +16,12 @@ Column {
     anchors.leftMargin: 90
     anchors.rightMargin: 90
     property alias label: label.text
-    property alias dataList: dropdown.dataList
-    property alias selectedValue: dropdown.selectedValue
+    property alias model: dropdown.model
+    property alias title: dropdown.title
+    property alias selectedTitle: dropdown.selectedTitle
+    property alias selectedIndex: dropdown.selectedIndex
 
-    signal selectionChanged (int index, variant data)
+    signal triggered (int index)
 
     Text {
         id: label
@@ -30,13 +32,11 @@ Column {
     }
     DropDown {
         id: dropdown
-        onSelectionChanged: root.selectionChanged(index, data)
-        delegateComponent: Component {
-            Text {
-                property variant data
-                x: 15
-                text: data
-            }
-        }
+        width: 400
+        minWidth: 400
+        maxWidth: 500
+        titleColor: "black"
+        replaceDropDownTitle: true
+        onTriggered: root.triggered(index)
     }
 }

settings/ManualScreen.qml
@@ -47,9 +47,9 @@ Item {
                         Item { width: 1; height: 1; },   // spacer
                         DropDownControl {
                             label: qsTr("Server type")
-                            dataList: Settings.serviceModel
-                            selectedValue: Settings.serviceName(emailAccount.recvType)
-                            onSelectionChanged: emailAccount.recvType = Settings.serviceCode(data)
+                            model: Settings.serviceModel
+                            selectedIndex: emailAccount.recvType
+                            onTriggered: emailAccount.recvType = index
                         },
                         TextControl {
                             id: recvServerField
@@ -67,9 +67,9 @@ Item {
                         },
                         DropDownControl {
                             label: qsTr("Security")
-                            dataList: Settings.encryptionModel
-                            selectedValue: Settings.encryptionName(emailAccount.recvSecurity)
-                            onSelectionChanged: emailAccount.recvSecurity = Settings.encryptionCode(data)
+                            model: Settings.encryptionModel
+                            selectedIndex: emailAccount.recvSecurity
+                            onTriggered: emailAccount.recvSecurity = index
                         },
                         TextControl {
                             id: recvUsernameField
@@ -109,15 +109,15 @@ Item {
                         DropDownControl {
                             id: sendAuthField
                             label: qsTr("Authentication")
-                            dataList: Settings.authenticationModel
-                            selectedValue: Settings.authenticationName(emailAccount.sendAuth)
-                            onSelectionChanged: emailAccount.sendAuth = Settings.authenticationCode(data)
+                            model: Settings.authenticationModel
+                            selectedIndex: emailAccount.sendAuth
+                            onTriggered: emailAccount.sendAuth = index
                         },
                         DropDownControl {
                             label: qsTr("Security")
-                            dataList: Settings.encryptionModel
-                            selectedValue: Settings.encryptionName(emailAccount.sendSecurity)
-                            onSelectionChanged: emailAccount.sendSecurity = Settings.encryptionCode(data)
+                            model: Settings.encryptionModel
+                            selectedIndex: emailAccount.sendSecurity
+                            onTriggered: emailAccount.sendSecurity = index
                         },
                         TextControl {
                             id: sendUsernameField

settings/settings.js
@@ -24,9 +24,9 @@ var serviceModel = [qsTr("POP"),
                     qsTr("IMAP")];
 
 function serviceName(code) {
-    if (code === "pop3")
+    if (code === "0")
         return serviceModel[0];
-    else if (code === "imap4") 
+    else if (code === "1")
         return serviceModel[1];
     else
         return "";
@@ -34,9 +34,9 @@ function serviceName(code) {
 
 function serviceCode(name) {
     if (name === serviceModel[0])
-        return "pop3";
+        return "0";
     else if (name === serviceModel[1])
-        return "imap4";
+        return "1";
     else
         return "";
 }

commit 8659a303c59b97917b88809ba25d352c557769dc

[Settings] fix bugs introduced by commit a7efd42298aec5550a77149eb480eaf48bc8ec4e

plugin/emailaccount.cpp
@@ -214,7 +214,7 @@ void EmailAccount::applyPreset()
 {
     switch(preset()) {
         case mobilemePreset:
-            setRecvType("imap4");
+            setRecvType("1");                    // imap
             setRecvServer("mail.me.com");
             setRecvPort("993");
             setRecvSecurity("1");                // SSL
@@ -228,7 +228,7 @@ void EmailAccount::applyPreset()
             setSendPassword(password());
             break;
         case gmailPreset:
-            setRecvType("imap4");
+            setRecvType("1");                    // imap
             setRecvServer("imap.gmail.com");
             setRecvPort("993");
             setRecvSecurity("1");                // SSL
@@ -242,7 +242,7 @@ void EmailAccount::applyPreset()
             setSendPassword(password());
             break;
         case yahooPreset:
-            setRecvType("imap4");
+            setRecvType("1");                    // imap
             setRecvServer("imap.mail.yahoo.com");
             setRecvPort("993");
             setRecvSecurity("1");                // SSL
@@ -256,7 +256,7 @@ void EmailAccount::applyPreset()
             setSendPassword(password());
             break;
         case aolPreset:
-            setRecvType("imap4");
+            setRecvType("1");                    // imap
             setRecvServer("imap.aol.com");
             setRecvPort("143");
             setRecvSecurity("0");                // none
@@ -270,7 +270,7 @@ void EmailAccount::applyPreset()
             setSendPassword(password());
             break;
         case mslivePreset:
-            setRecvType("pop3");
+            setRecvType("0");                    // pop
             setRecvServer("pop3.live.com");
             setRecvPort("995");
             setRecvSecurity("1");                // SSL
@@ -284,7 +284,7 @@ void EmailAccount::applyPreset()
             setSendPassword(password());
             break;
         case noPreset:
-            setRecvType("imap4");
+            setRecvType("1");                    // imap
             setRecvPort("993");
             setRecvSecurity("1");                // SSL
             setRecvUsername(username());         // username only

plugin/emailaccountsettingsmodel.cpp
@@ -258,10 +258,12 @@ bool EmailAccountSettingsModel::setData(const QModelIndex &index, const QVariant
             case RecvTypeRole:
                 // prevent bug where recv type gets reset
                 // when loading the first time
-                if (value.toInt() == 0) {
+                if (value.toString() == "0") {
                     newrecvsvc = "pop3";
-                } else if (value.toInt() == 1) {
+                } else if (value.toString() == "1") {
                     newrecvsvc = "imap4";
+                } else {
+                    return false;
                 }
                 if (newrecvsvc == recvsvc) {
                     return true;