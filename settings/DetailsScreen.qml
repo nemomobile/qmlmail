nemomobile/qmlmail

commit 9fb47c1783f17951c86cf69fa67d19cd0c826137

[Settings] MeeGo.Components conversion WIP
Converted AccountExpandobox, AccountSettings, ConfirmScreen,
ControlGroup, DetailsScreen.
Removed Label due to namespace conflict with existing component.

settings/DetailsScreen.qml
@@ -7,7 +7,7 @@
  */
 
 import Qt 4.7
-import MeeGo.Labs.Components 0.1
+import MeeGo.Components 0.1
 import MeeGo.Settings 0.1
 import "settings.js" as Settings
 
@@ -38,133 +38,92 @@ Item {
                 anchors.right: parent.right
                 anchors.margins: 20
                 spacing: 10
-                Label {
+                Text {
                     font.weight: Font.Bold
                     text: qsTr("Account details")
                 }
-                Label {
+                Text {
                     text: qsTr("Account: %1").arg(emailAccount.description)
                 }
-                Label {
+                Text {
                     text: qsTr("Name: %1").arg(emailAccount.name)
                 }
-                Label {
+                Text {
                     text: qsTr("Email address: %1").arg(emailAccount.address)
                 }
                 Item {
                     width: 1
                     height: 20
                 }
-                Label {
+                Text {
                     text: qsTr("Receiving:")
                 }
-                Label {
+                Text {
                     text: qsTr("Server type: %1").arg(Settings.serviceName(emailAccount.recvType))
                 }
-                Label {
+                Text {
                     text: qsTr("Server address: %1").arg(emailAccount.recvServer)
                 }
-                Label {
+                Text {
                     text: qsTr("Port: %1").arg(emailAccount.recvPort)
                 }
-                Label {
+                Text {
                     text: qsTr("Security: %1").arg(Settings.encryptionName(emailAccount.recvSecurity))
                 }
-                Label {
+                Text {
                     text: qsTr("Username: %1").arg(emailAccount.recvUsername)
                 }
                 Item {
                     width: 1
                     height: 20
                 }
-                Label {
+                Text {
                     text: qsTr("Sending:")
                 }
-                Label {
+                Text {
                     text: qsTr("Server address: %1").arg(emailAccount.sendServer)
                 }
-                Label {
+                Text {
                     text: qsTr("Port: %1").arg(emailAccount.sendPort)
                 }
-                Label {
+                Text {
                     text: qsTr("Authentication: %1").arg(Settings.authenticationName(emailAccount.sendAuth))
                 }
-                Label {
+                Text {
                     text: qsTr("Security: %1").arg(Settings.encryptionName(emailAccount.sendSecurity))
                 }
-                Label {
+                Text {
                     text: qsTr("Username: %1").arg(emailAccount.sendUsername)
                 }
             }
         }
     }
-    Component {
+    ModalDialog {
         id: verifyCancel
-        ModalDialog {
-            property variant settingsPage
-            leftButtonText: qsTr ("Yes")
-            rightButtonText: qsTr ("No")
-            dialogTitle: qsTr ("Discard changes")
-            contentLoader.sourceComponent: DialogText {
-                text: qsTr ("You have made changes to your settings, are you sure you want to cancel?")
-            }
-
-            onDialogClicked: {
-                dialogLoader.sourceComponent = undefined;
-                if (button == 1) {
-                    settingsPage.state = settingsPage.getHomescreen()
-                }
-            }
+        acceptButtonText: qsTr ("Yes")
+        cancelButtonText: qsTr ("No")
+        title: qsTr ("Discard changes")
+        content: Text {
+            text: qsTr ("You have made changes to your settings, are you sure you want to cancel?")
         }
+        onAccepted: { settingsPage.state = settingsPage.getHomescreen() }
     }
-    Component {
+    ModalDialog {
         id: errorDialog
-        ModalDialog {
-            property variant settingsPage
-            property string errorMessage
-            property int errorCode
-            rightButtonText: qsTr("OK")
-            dialogTitle: qsTr("Error")
-            contentLoader.sourceComponent: DialogText {
-                text: qsTr("Error %1: %2").arg(errorCode).arg(errorMessage)
-            }
-            onDialogClicked: {
-                dialogLoader.sourceComponent = undefined;
-                settingsPage.state = "ManualScreen";
-                loader.item.message = qsTr("Sorry, we can't automatically set up your account. Please fill in account details:");
-            }
+        acceptButtonText: qsTr("OK")
+        showCancelButton: false
+        title: qsTr("Error")
+        content: Text {
+            text: qsTr("Error %1: %2").arg(errorCode).arg(errorMessage)
+        }
+        onAccepted: {
+            settingsPage.state = "ManualScreen";
+            loader.item.message = qsTr("Sorry, we can't automatically set up your account. Please fill in account details:");
         }
     }
 
     // spinner overlay
-    TopItem { id: topItem }
-    Component {
-        id: spinnerComponent
-        Item {
-            anchors.fill: parent
-            Rectangle {
-                anchors.fill: parent
-                color: "black"
-                opacity: 0.7
-            }
-            Text {
-                anchors.horizontalCenter: parent.horizontalCenter
-                y: parent.height / 4
-                font.pixelSize: theme_fontPixelSizeLarge
-                color: "white"
-                text: qsTr("Testing account configuration...")
-            }
-            Spinner {
-                id: spinner
-                spinning: true
-                maxSpinTime: 3600000
-            }
-            MouseArea {
-                anchors.fill: parent
-            }
-        }
-    }
-
+    Spinner { id: spinner }
 
     //FIXME use standard action bar here
     Rectangle {
@@ -181,25 +140,21 @@ Item {
             height: 45
             anchors.margins: 10
             //color: "white"
-            title: qsTr("Next")
+            text: qsTr("Next")
             onClicked: {
                 emailAccount.save();
                 emailAccount.test();
-                overlay = spinnerComponent.createObject(root);
-                overlay.parent = topItem.topItem;
+                spinner.show();
             }
             Connections {
                 target: emailAccount
                 onTestSucceeded: {
-                    spinnerComponent.destroy();
+                    spinner.hide();
                     settingsPage.state = "ConfirmScreen";
                 }
                 onTestFailed: {
-                    spinnerComponent.destroy();
-                    showModalDialog(errorDialog);
-                    dialogLoader.item.settingsPage = settingsPage;
-                    dialogLoader.item.errorMessage = emailAccount.errorMessage;
-                    dialogLoader.item.errorCode = emailAccount.errorCode;
+                    spinner.hide();
+                    errorDialog.show();
                     emailAccount.remove();
                 }
             }
@@ -211,10 +166,10 @@ Item {
             height: 45
             anchors.margins: 10
             //color: "white"
-            title: qsTr("Manual Edit")
+            text: qsTr("Manual Edit")
             onClicked: {
-                    settingsPage.state = "ManualScreen";
-                    loader.item.message = qsTr("Please fill in account details:");
+                settingsPage.state = "ManualScreen";
+                loader.item.message = qsTr("Please fill in account details:");
             }
         }
         Button {
@@ -224,10 +179,9 @@ Item {
             height: 45
             anchors.margins: 10
             //color: "white"
-            title: qsTr("Cancel")
+            text: qsTr("Cancel")
             onClicked: {
-                showModalDialog(verifyCancel);
-                dialogLoader.item.settingsPage = settingsPage;
+                verifyCancel.show();
             }
         }
     }

settings/AccountExpandobox.qml
@@ -7,7 +7,7 @@
  */
 
 import Qt 4.7
-import MeeGo.Labs.Components 0.1
+import MeeGo.Components 0.1
 import "settings.js" as Settings
 
 Expandobox {
@@ -211,37 +211,25 @@ Expandobox {
                 anchors.horizontalCenter: parent.horizontalCenter
                 height: 45
                 width: 300
-                title: qsTr("Delete Account")
+                text: qsTr("Delete Account")
                 bgSourceUp: "image://theme/btn_red_up"
                 bgSourceDn: "image://theme/btn_red_dn"
-                onClicked: {
-                    showModalDialog(verifyDelete);
-                    dialogLoader.item.settingsPage = settingsPage;
-                    dialogLoader.item.index = index;
-                }
+                onClicked: { verifyDelete.show(); }
             }
             Item { width: 1; height: 20; }
         }
         }
     }
-    Component {
+    ModalDialog {
         id: verifyDelete
-        ModalDialog {
-            property variant settingsPage
-            property int index
-            leftButtonText: qsTr ("Yes")
-            rightButtonText: qsTr ("Cancel")
-            dialogTitle: qsTr ("Delete account")
-            contentLoader.sourceComponent: DialogText {
-                text: qsTr ("Are you sure you want to delete this account?")
-            }
-
-            onDialogClicked: {
-                dialogLoader.sourceComponent = undefined;
-                if (button == 1) {
-                    settingsPage.accountSettingsModel.deleteRow(index);
-                }
-            }
+        acceptButtonText: qsTr ("Yes")
+        cancelButtonText: qsTr ("Cancel")
+        title: qsTr ("Delete account")
+        content: Text {
+            text: qsTr ("Are you sure you want to delete this account?")
+        }
+        onAccepted: {
+            settingsPage.accountSettingsModel.deleteRow(index);
         }
     }
 }

settings/AccountSettings.qml
@@ -7,7 +7,7 @@
  */
 
 import Qt 4.7
-import MeeGo.Labs.Components 0.1
+import MeeGo.Components 0.1
 import "settings.js" as Settings
 
 Item {
@@ -136,20 +136,12 @@ Item {
                         anchors.right: parent.right
                         color: "#eaf6fb"
                         height: 100
-                        BorderImage {
+                        TextField {
+                            id: signature
                             anchors.fill: parent
                             anchors.margins: 10
-                            border { top: 10; bottom: 10; left: 10; right: 10 }
-                            source: "image://theme/email/frm_textfield_l"
-                            TextEdit {
-                                id: signature
-                                anchors.fill: parent
-                                anchors.margins: 10
-                                font.pixelSize: theme_fontPixelSizeLarge
-                                text: accountSettingsModel.signature()
-                                onTextChanged: accountSettingsModel.setSignature(text)
-                                CCPContextArea { editor: parent }
-                            }
+                            text: accountSettingsModel.signature()
+                            onTextChanged: accountSettingsModel.setSignature(text)
                         }
                     }
                 }
@@ -197,34 +189,23 @@ Item {
             }
         }
     }
-    Component {
+    ModalDialog {
         id: verifyCancel
-        ModalDialog {
-            property variant settingsPage
-            leftButtonText: qsTr ("Yes")
-            rightButtonText: qsTr ("No")
-            dialogTitle: qsTr ("Discard changes")
-            contentLoader.sourceComponent: DialogText {
-                text: qsTr ("You have made changes to your settings, are you sure you want to cancel?")
-            }
-
-            onDialogClicked: {
-                dialogLoader.sourceComponent = undefined;
-                if (button == 1) {
-                    settingsPage.returnToEmail();
-                }
-            }
+        acceptButtonText: qsTr ("Yes")
+        cancelButtonText: qsTr ("No")
+        title: qsTr ("Discard changes")
+        content: Text {
+            text: qsTr ("You have made changes to your settings, are you sure you want to cancel?")
         }
+        onAccepted: { settingsPage.returnToEmail(); }
     }
-    Component {
+    ModalDialog {
         id: changesSaved
-        ModalDialog {
-            leftButtonText: qsTr ("OK")
-            dialogTitle: qsTr ("Changes saved")
-            contentLoader.sourceComponent: DialogText {
-                text: qsTr ("Your changes have been saved.")
-            }
-            onDialogClicked: { dialogLoader.sourceComponent = undefined; }
+        acceptButtonText: qsTr ("OK")
+        showCancelButton: false
+        title: qsTr ("Changes saved")
+        content: Text {
+            text: qsTr ("Your changes have been saved.")
         }
     }
     //FIXME use standard action bar here
@@ -241,14 +222,14 @@ Item {
             height: 45
             anchors.margins: 10
             //color: "white"
-            title: qsTr("Save changes")
+            text: qsTr("Save changes")
             onClicked: {
                 accountSettingsModel.saveChanges();
                 // cdata will be set if called from email app
                 if (scene.applicationData) {
                     settingsPage.returnToEmail();
                 } else {
-                    showModalDialog(changesSaved);
+                    changesSaved.show();
                 }
             }
         }
@@ -259,10 +240,9 @@ Item {
             height: 45
             anchors.margins: 10
             //color: "white"
-            title: qsTr("Cancel")
+            text: qsTr("Cancel")
             onClicked: {
-                showModalDialog(verifyCancel);
-                dialogLoader.item.settingsPage = settingsPage;
+                verifyCancel.show();
             }
         }
     }

settings/ConfirmScreen.qml
@@ -7,7 +7,7 @@
  */
 
 import Qt 4.7
-import MeeGo.Labs.Components 0.1
+import MeeGo.Components 0.1
 import MeeGo.Settings 0.1
 
 Item {
@@ -46,7 +46,7 @@ Item {
                     width: 300
                     //font: "Droid Sans"
                     //color: "white"
-                    title: qsTr("Done")
+                    text: qsTr("Done")
                     onClicked: settingsPage.returnToEmail()
                 }
             }

settings/ControlGroup.qml
@@ -7,7 +7,6 @@
  */
 
 import Qt 4.7
-import MeeGo.Labs.Components 0.1
 
 Column {
     property alias title: titleText.text

settings/Label.qml
@@ -1,15 +1,0 @@
-/*
- * Copyright 2011 Intel Corporation.
- *
- * This program is licensed under the terms and conditions of the
- * Apache License, version 2.0.  The full text of the Apache License is at 	
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-
-import Qt 4.7
-import MeeGo.Labs.Components 0.1
-
-Text {
-    font.pixelSize: theme_fontPixelSizeLarge
-    //color: "white"
-}

commit 5f3a86be61f63b9830d4b41ccb7af49ab4c0b788

[Settings] fix empty error dialog box on details screen
bug introduced by commit 9fb47c1783f17951c86cf69fa67d19cd0c826137

settings/DetailsScreen.qml
@@ -114,7 +114,7 @@ Item {
         showCancelButton: false
         title: qsTr("Error")
         content: Text {
-            text: qsTr("Error %1: %2").arg(errorCode).arg(errorMessage)
+            text: qsTr("Error %1: %2").arg(emailAccount.errorCode).arg(emailAccount.errorMessage)
         }
         onAccepted: {
             settingsPage.state = "ManualScreen";